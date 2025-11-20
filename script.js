b0ace6978b91007a9bd5a1e66949574ca7f43c54
AhmedMensi2005 <amensi705@gmail.com>
2025-11-20 17:14:40 +0100

    ajout des information secondaire et reglage leur style du style

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 function pushdata(data){
+    const uvSuggestions = {
+        0: "Minimal risk. No protection needed.",
+        1: "Minimal risk. Sunglasses are enough.",
+        2: "Low risk. Wear sunglasses when outside.",
+        3: "Moderate risk. Use sunscreen (SPF 30+).",
+        4: "Moderate risk. Use sunscreen and stay hydrated.",
+        5: "Moderate risk. Wear a hat and sunglasses.",
+        6: "High risk. Use SPF 30+, seek shade at midday.",
+        7: "High risk. Wear protective clothing and sunscreen.",
+        8: "Very high risk. Reduce sun exposure between 11am–4pm.",
+        9: "Very high risk. Use SPF 50+ and stay in shade.",
+        10: "Very high risk. Avoid direct sun as much as possible.",
+        11: "Extreme risk. Stay indoors during peak hours!",
+        12: "Extreme risk. UV extremely dangerous, avoid exposure."
+    };
     console.log(data);
     let icon = document.getElementById("icon");
     console.log(icon.src)
     const date = new Date();
     console.log(date);
     icon.src = data.current.condition.icon;
-    document.getElementById("TEMPERATURE").innerText=data.current.dewpoint_c+"°C";
+    document.getElementById("TEMPERATURE").innerText=data.current.heatindex_c+"°C";
     document.getElementById("PRECIPITATION").innerText=data.current.precip_mm+"mm";
     document.getElementById("HUMIDITY").innerText=data.current.humidity+"%";
     document.getElementById("VISIBILITY").innerText=data.current.vis_km+"km";
+    document.getElementById("weatherStatus").innerText=data.current.condition.text;
     document.getElementById("jour").innerText=date.toLocaleDateString("en-US", { weekday: "long" });
     document.getElementById("date").innerText=date.getFullYear()+"/"+(date.getMonth() + 1)+"/"+date.getDate()
-    
+    document.getElementById("windspeed").innertext=data.current.wind_kph+"kmh"
+    document.getElementById("winddirection").innerText="direction: "+data.current.wind_dir
+    document.getElementById("uvvalue").innerText=data.current.uv
+    document.getElementById("uvsuggestion").innerText=uvSuggestions[data.current.uv]
+
     
 }
 
